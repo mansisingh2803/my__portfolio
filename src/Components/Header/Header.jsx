1c6298645a67ee16b78795aac0524ed6576704cd
Mansi Singh <66991274+mansisingh2803@users.noreply.github.com>
2023-07-04 16:42:51 +0530

    Update Header.jsx
    container

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 
     return (
         <header className='header'>
-            <nav className='nav container'>
+            <nav className='nav'>
                 <a href='index.html' className='nav__logo'>
                     Portfolio...
                 </a>
